0c91a525880bbc117c74c73772ac0ebb814a1ca4
Julien Boulen <julien.boulen@univ-rennes2.fr>
2023-02-09 15:40:19 +0100

    fix(string): remove string concatenation
    Closes #1

--- lang/en/local_debugtoolbar.php
+++ lang/en/local_debugtoolbar.php
@@ -65,8 +65,8 @@ $string['site_admin_X'] = 'Site admin: {$a}';
 $string['strings_filtered_X'] = 'Strings filtered: {$a}';
 $string['subpage_X'] = 'Sub-page: {$a}';
 $string['total_time_X'] = 'Total time: {$a} secs';
-$string['usage_warning'] = '<strong>DO NOT ENABLE THIS PLUGIN IN A PRODUCTION ENVIRONMENT.</strong><br />'.
-    'This plugin will override the following variables : <code>$CFG->debug</code>, <code>$CFG->debugdisplay</code> and <code>$CFG->perfdebug</code>.';
+$string['usage_warning'] = '<strong>DO NOT ENABLE THIS PLUGIN IN A PRODUCTION ENVIRONMENT.</strong><br />
+This plugin will override the following variables : <code>$CFG->debug</code>, <code>$CFG->debugdisplay</code> and <code>$CFG->perfdebug</code>.';
 $string['version_X'] = 'Version: {$a}';
 $string['warnings_X'] = 'Warnings: {$a}';
 $string['X_queries_in_Y_secs'] = '{$a->queries} queries in {$a->time} secs';
