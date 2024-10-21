da9c09c83902a6cce2866b849251ecd19a85d0c4
Julien Boulen <julien.boulen@univ-rennes2.fr>
2024-10-21 15:48:53 +0200

    feat(core): release 1.3.0

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024100300;
-$plugin->release = '1.2.1';
+$plugin->version = 2024102100;
+$plugin->release = '1.3.0';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->requires = 2024042200; // Moodle 4.4 release and upwards.
+$plugin->requires = 2024100700; // Moodle 4.5 release and upwards.
 $plugin->component = 'local_debugtoolbar';
